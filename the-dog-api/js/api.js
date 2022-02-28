8ada10ab70fcc5c99bb86b3381cbba9ff7369b3e
MDABDULLAH16 <mdabdullah161036@gmail.com>
2022-02-28 13:23:55 +0600

    Dog api added

--- the-dog-api/js/api.js
+++ the-dog-api/js/api.js
@@ -5,5 +5,23 @@ const loadDog = () => {
         .then(data => displayDog(data))
 }
 const displayDog = (dogList) => {
-    console.log(dogList);
+    const main = document.getElementById('main');
+    const first10Data = dogList.slice(0, 100);
+
+    for (const dog of first10Data) {
+        console.log(dog.weight)
+        const div = document.createElement('div');
+        div.className = "col-lg-4"
+        div.innerHTML = `
+        <h2>${dog.name}</h2>
+        <p>${dog.temperament}</p>
+        <h4>${dog.weight.imperial}</h4>
+
+        <img wight="400px" height="250px" src="${dog.image.url}" alt="">
+        `
+
+
+        main.appendChild(div)
+
+    }
 }
